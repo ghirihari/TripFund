a359414459eb4a71d1e8d1b133637c9d6e4e989f
ghirihari <ghirihari@gmail.com>
2020-09-19 12:06:18 +0530

    Updates

---
+++ precache-manifest.087bf57b66bb9f18cf877c3e634f5d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f9549286540d4e8aa5fce88c65b124",
+    "url": "/TripFund/index.html"
+  },
+  {
+    "revision": "08d38a37c0aa2776d625",
+    "url": "/TripFund/static/css/main.20c24073.chunk.css"
+  },
+  {
+    "revision": "6feff2794a11e17f86b3",
+    "url": "/TripFund/static/js/2.0d8926ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TripFund/static/js/2.0d8926ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d38a37c0aa2776d625",
+    "url": "/TripFund/static/js/main.92165d4d.chunk.js"
+  },
+  {
+    "revision": "a69012280b4bc732c10b",
+    "url": "/TripFund/static/js/runtime-main.4c683c1a.js"
+  }
+]);
